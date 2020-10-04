05b29cf7baaa1979f66f6d6accf3bed84a71d2ca
kc emmanuel <fidekg123@gmail.com>
2020-10-04 05:56:04 +0100

    final commit

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    apiKey: "AIzaSyAzSGdRfi3ciEiqbhB2GVbnESe7_FVy738",
+    authDomain: "oriamoapp-86ef0.firebaseapp.com",
+    databaseURL: "https://oriamoapp-86ef0.firebaseio.com",
+    projectId: "oriamoapp-86ef0",
+    storageBucket: "oriamoapp-86ef0.appspot.com",
+    messagingSenderId: "429405031223",
+    appId: "1:429405031223:web:7124649c41227face5a8b2",
+    measurementId: "G-E9BWHK2DB5"
+  };
